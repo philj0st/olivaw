2b45f6c0c1cd2cb603f41d3fce11a816a1412448
Philipp Jost <philipp.jost@protonmail.com>
2021-11-26 17:59:36 +0900

    feat: LiveData Transformations as properties

--- DataBindings/app/src/main/java/com/example/databindings/Calc.kt
+++ DataBindings/app/src/main/java/com/example/databindings/Calc.kt
@@ -3,13 +3,24 @@ package com.example.databindings
 import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
+
+const val QUARTER = 25
+const val DIME = 10
+val NICKEL = 5
+val PENNY = 1
 
 class Calc : ViewModel(){
 
     private val _amount =  MutableLiveData(0)
     val amount: LiveData<Int> = _amount
 
+    // dimes is exposed as LiveData using a Transformation instead of a @Bindable property.
+    val dimes: LiveData<Int> = Transformations.map(_amount) {
+        // whatever cannot be turned into quarters we will turn into dimes.
+        (it % QUARTER) % DIME
+    }
 
     // add digits 0-9
     fun appendDigit(digit:Int){
